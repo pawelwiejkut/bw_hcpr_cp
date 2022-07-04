c1e8ed37cb6017719f993a260b9ef48886d56fab
Paweł Wiejkut <poczta@pawelwiejkut.net>
2022-07-04 16:52:36 +0200

    Create deploy-merged-build.sh

---
+++ ci/deploy-merged-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Build merged
+npm run merge || exit 1
+wc -l ./zbw_bw_hcpr_cp.abap
+
+# Deploy artifacts
+git clone https://github.com/pawelwiejkut/bw_trfn_tester.git
+cp zbw_hcpr_cp.abap bw_hcpr_cp/last_build/zbw_hcpr_cp.abap
+cd bw_hcpr_cp
+
+# Commit
+git status
+git config user.email "ci@pawelwiejkut.net"
+git config user.name "CI"
+git add last_build/zbw_trfn_tester.abap
+git commit -m "CI build [skip ci]" || exit 1
+git push -q https://$GITHUB_API_KEY@github.com/pawelwiejkut/bw_trfn_tester.git 
